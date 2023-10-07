3578f60dbe8c6223bc490f41739e0ee8cdbcb779
Kasper Seweryn <github@wvffle.net>
2023-10-08 00:37:51 +0200

    feat: add auth store

---
+++ composables/fetch.ts
@@ -0,0 +1,20 @@
+const runtimeConfig = useRuntimeConfig()
+const baseURL = runtimeConfig.api.baseUrl ?? ''
+
+export const $fetchAPI = $fetch.create({
+  baseURL,
+})
+
+export const useFetchAPI: typeof useFetch = (url, options) => {
+  return useFetch(url, {
+    baseURL,
+    ...options,
+  })
+}
+
+export const useLazyFetchAPI: typeof useLazyFetch = (url, options) => {
+  return useLazyFetch(url, {
+    baseURL,
+    ...options,
+  })
+}
