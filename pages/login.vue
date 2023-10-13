6cd8af9dc3411c55cb620762b554d52b808c37a4
jwroblewska <hortex.paw@gmail.com>
2023-10-13 11:48:55 +0200

    feat: update auth views

--- pages/login.vue
+++ pages/login.vue
@@ -5,13 +5,25 @@ definePageMeta({
 </script>
 
 <template>
-    <UFormGroup label="E-mail" required name="email" class="scale-90 ">
+  <div class="min-w-[450px] mx-auto">
+    <UFormGroup label="E-mail" required name="email" class="mb-1">
       <UInput placeholder="you@example.com" icon="i-heroicons-envelope" />
     </UFormGroup>
-    <UFormGroup label="Hasło" required name="password" help="Nie pamiętasz hasła?" class="scale-90">
+    <UFormGroup label="Hasło" required name="password" class="mb-1">
       <UInput type="password" />
+      <ULink to="/reset-password" class="mt-2 text-primary text-sm hover:underline">
+        Nie pamiętasz hasła?
+      </ULink>
     </UFormGroup>
-    <UButton type="submit" class="mt-1 mx-auto min-w-[400px] justify-center scale-90">
+    <UButton type="submit" class="mt-20 mx-auto" block>
       Zaloguj się
     </UButton>
+    <UAlert title="Nie posiadasz konta?" class="mt-5">
+      <template #description>
+        <ULink to="/register" class="text-primary text-sm hover:underline">
+          Zarejestruj się!
+        </ULink>
+      </template>
+    </UAlert>
+  </div>
 </template>
