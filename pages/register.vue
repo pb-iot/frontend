32b18f47e897721f53ff9ca85ec08dc59a0c73e1
SiwyKot41 <frutkowski80@gmail.com>
2023-10-17 14:56:47 +0200

    config: add eslint with configuration files

--- pages/register.vue
+++ pages/register.vue
@@ -6,27 +6,65 @@ definePageMeta({
 
 <template>
   <div class="min-w-[450px] mx-auto">
-    <UFormGroup label="Imie" required name="name" class="mb-1">
-      <UInput class="normal-case"/>
+    <UFormGroup
+      label="Imie"
+      required
+      name="name"
+      class="mb-1"
+    >
+      <UInput class="normal-case" />
     </UFormGroup>
-    <UFormGroup label="Nazwisko" required name="surname" class="mb-1">
+    <UFormGroup
+      label="Nazwisko"
+      required
+      name="surname"
+      class="mb-1"
+    >
       <UInput />
     </UFormGroup>
-    <UFormGroup label="E-mail" required name="email" class="mb-1">
-      <UInput placeholder="you@example.com" icon="i-heroicons-envelope" />
+    <UFormGroup
+      label="E-mail"
+      required
+      name="email"
+      class="mb-1"
+    >
+      <UInput
+        placeholder="you@example.com"
+        icon="i-heroicons-envelope"
+      />
     </UFormGroup>
-    <UFormGroup label="Hasło" required name="password" class="mb-1">
+    <UFormGroup
+      label="Hasło"
+      required
+      name="password"
+      class="mb-1"
+    >
       <UInput type="password" />
     </UFormGroup>
-    <UFormGroup label="Powtórz hasło" required name="password" class="mb-1">
+    <UFormGroup
+      label="Powtórz hasło"
+      required
+      name="password"
+      class="mb-1"
+    >
       <UInput type="password" />
     </UFormGroup>
-    <UButton type="submit" class="mt-20 mx-auto" block>
+    <UButton
+      type="submit"
+      class="mt-20 mx-auto"
+      block
+    >
       Zarejestruj się
     </UButton>
-    <UAlert title="Posiadasz już konto?" class="mt-5">
+    <UAlert
+      title="Posiadasz już konto?"
+      class="mt-5"
+    >
       <template #description>
-        <ULink to="/login" class="text-primary text-sm hover:underline">
+        <ULink
+          to="/login"
+          class="text-primary text-sm hover:underline"
+        >
           Zaloguj się!
         </ULink>
       </template>
