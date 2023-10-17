e27f95a03437e27becd1909fc76a30f382d15ef8
wvffle <github@wvffle.net>
2023-10-17 15:05:49 +0000

    style: lint all components

--- composables/fetch.ts
+++ composables/fetch.ts
@@ -2,19 +2,19 @@ const runtimeConfig = useRuntimeConfig()
 const baseURL = runtimeConfig.api.baseUrl ?? ''
 
 export const $fetchAPI = $fetch.create({
-  baseURL,
+  baseURL
 })
 
 export const useFetchAPI: typeof useFetch = (url, options) => {
   return useFetch(url, {
     baseURL,
-    ...options,
+    ...options
   })
 }
 
 export const useLazyFetchAPI: typeof useLazyFetch = (url, options) => {
   return useLazyFetch(url, {
     baseURL,
-    ...options,
+    ...options
   })
 }
