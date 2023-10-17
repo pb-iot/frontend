794bdfb1c8da18dd86385fde2a7d2880690b9baa
jwroblewska <hortex.paw@gmail.com>
2023-10-17 13:26:21 +0200

    add: pages reset&change password

---
+++ pages/change-password.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'auth'
+})
+</script>
+
+<template>
+  <div class="min-w-[450px] mx-auto">
+    <UFormGroup label="Nowe hasło" required name="password" class="mb-1">
+      <UInput type="password" />
+    </UFormGroup>
+    <UFormGroup label="Powtórz hasło" required name="password" class="mb-1">
+      <UInput type="password" />
+    </UFormGroup>
+    <UButton type="submit" class="mt-20 mx-auto" block>
+      Zmień hasło
+    </UButton>
+  </div>
+</template>
--- pages/reset-password.vue
+++ pages/reset-password.vue
@@ -5,5 +5,17 @@ definePageMeta({
 </script>
 
 <template>
-  
+  <div class="min-w-[450px] mx-auto">
+    <UAlert 
+      title="Podaj adres e-mail, którego użyto do założenia konta w naszym serwisie. Po zatwierdzeniu formularza wyślemy na niego wiadomość z linkiem do zmiany hasła." 
+      color="primary"
+      variant="soft"
+      class="text-center"/>
+    <UFormGroup label="E-mail" required name="email" class="mt-4">
+      <UInput placeholder="you@example.com" icon="i-heroicons-envelope" />
+    </UFormGroup>
+    <UButton type="submit" class="mt-16 mx-auto" block>
+      Resetuj hasło
+    </UButton>
+  </div>
 </template>
