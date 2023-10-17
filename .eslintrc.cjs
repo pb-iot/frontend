b3b637084ff6f00c4bb173abb2da192716bbaec0
wvffle <github@wvffle.net>
2023-10-17 15:02:17 +0000

    style: lint all files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,16 @@
 /* eslint-env node */
 module.exports = {
-    extends: [
-      // 'plugin:@typescript-eslint/recommended',
-      // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      // 'plugin:@typescript-eslint/strict',
-      'standard',
-      '@nuxt/eslint-config'
-    ],
-    // parser: '@typescript-eslint/parser',
-    // parserOptions: {
-    //   project: './tsconfig.eslint.json',
-    //   tsconfigRootDir: __dirname,
-    //   extraFileExtensions: ['.vue']
-    // },
-    // plugins: ['@typescript-eslint'],
-    root: true,
-    overrides: [
-      {
-        files: ['*.ts', '*.vue'],
-        rules: {
-          'no-undef': 'off'
-        }
+  extends: [
+    'standard',
+    '@nuxt/eslint-config'
+  ],
+  root: true,
+  overrides: [
+    {
+      files: ['*.ts', '*.vue'],
+      rules: {
+        'no-undef': 'off'
       }
-    ]
-  }
+    }
+  ]
+}
