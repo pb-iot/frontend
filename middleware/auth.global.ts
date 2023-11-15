4b455b52c4c5ddf6636b243c6266dbc513170196
Kasper Seweryn <github@wvffle.net>
2023-11-15 13:29:35 +0100

    feat: add route middlewares

---
+++ middleware/auth.global.ts
@@ -0,0 +1,14 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  const auth = useAuthStore()
+
+  // When not logged in and not navigates to auth route, redirect to login
+  if (!auth.isLoggedIn && !to.meta.layout === 'auth') {
+    return navigateTo('/')
+  } 
+
+  // When logged in and navigates to auth route, redirect to dashboard
+  if (auth.isLoggedIn && to.meta.layout === 'auth') {
+    return navigateTo('/dashboard')
+  }
+})
+
