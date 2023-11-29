1850fc15dde1fe4ca0631461167113829eb20b14
OlaWrobe <beliisa96@gmail.com>
2023-11-29 12:02:45 +0100

    refactor: create profile card component

---
+++ components/profile/Card.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+defineProps<{
+    // TODO: Use `User` type
+    user: {
+        firstname: string
+        lastname: string
+        avatar: string
+    }
+    onCancel?:() => void | Promise<void>
+    onConfirm?: () => void | Promise<void>
+}>()
+</script>
+
+<template>
+  <UCard>
+    <template #header>
+      <div class="flex items-center mb-4 h-20">
+        <UAvatar
+          class="mr-2"
+          size="2xl"
+          :src="user.avatar"
+          alt="Avatar"
+        />
+        <p class="text-lg font-semibold">
+          {{ user.firstname }} {{ user.lastname }}
+        </p>
+      </div>
+    </template>
+
+    <slot />
+
+    <!-- Buttons for actions -->
+    <template
+      v-if="$slots.actions"
+      #footer
+    >
+      <div class="flex justify-between mt-5 mx-1">
+        <slot name="actions" />
+      </div>
+    </template>
+    <template
+      v-else-if="onCancel && onConfirm"
+      #footer
+    >
+      <div class="flex justify-between mt-5 mx-1">
+        <UButton
+          type="button"
+          class="flex-1 place-content-center"
+          color="red"
+          variant="soft"
+          label="Anuluj"
+          @click="onCancel"
+        />
+        <UButton
+          type="submit"
+          class="flex-1 ml-4 place-content-center"
+          label="Zapisz zmiany"
+          @click="onConfirm"
+        />
+      </div>
+    </template>
+  </UCard>
+</template>
--- pages/dashboard/greenhouse/[greenhouseId].vue
+++ pages/dashboard/greenhouse/[greenhouseId].vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
+import type { UButton } from '#ui-colors/components';
 import { useRoute } from 'vue-router'
 const route = useRoute()
 const id = computed(() => route.params.greenhouseId)
+
+const user = {
+  firstname: 'Ola',
+  lastname: 'Wrobel',
+  avatar: 'https://avatars.githubusercontent.com/u/739984?v=4'
+}
 </script>
 
 <template>
-  <div class="my-greenhouse">
-    <h1>My Greenhouse: {{ id }}</h1>
-    <ProfileOptionsAndInfo />
-    <ProfileChangeData />
-    <ProfileChangePassword />
-    <AlertDelete />
-    <AlertChangePassword />
+  <div class="grid grid-cols-2 gap-8">
+    <div>
+      <h1>My Greenhouse: {{ id }}</h1>
+      <ProfileChangePassword :user="user" />
+    </div>
   </div>
 </template>
