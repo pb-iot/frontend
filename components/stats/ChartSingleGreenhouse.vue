2d15f3c907151d962fc2825325a426cb480f0d1d
jwroblewska <hortex.paw@gmail.com>
2024-01-30 20:35:19 +0100

    stats components fix

---
+++ components/stats/ChartSingleGreenhouse.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <StatsCard>
+    <!---WYKRES HERE-->
+  </StatsCard>
+</template>
--- pages/dashboard/greenhouse/[greenhouseId].vue
+++ pages/dashboard/greenhouse/[greenhouseId].vue
@@ -1,21 +1,37 @@
 <script setup lang="ts">
-import type { UButton } from '#ui-colors/components';
+import type { UButton } from '#ui-colors/components'
 import { useRoute } from 'vue-router'
 const route = useRoute()
 const id = computed(() => route.params.greenhouseId)
-const dataForTitle = "xd"
+const dataForTitle = 'xd'
+const averageTemperature = '1'
 const user = {
   firstname: 'Ola',
   lastname: 'Wrobel',
   avatar: 'https://avatars.githubusercontent.com/u/739984?v=4'
 }
+const greenhouseStats = {
+  averageTemperature: '1',
+  averageAirHumidity: '2',
+  averageLightLevel: '1',
+  averagePAR: '1',
+  averageCO2Level: '1',
+  averageSoilWaterLevel: '1',
+  averageSoilECLevel: '1',
+  averageSoilTemperature: '1',
+  averageSoilAndPlantWeight: '1',
+  averageStemMicroVariability: '2'
+}
+
 </script>
 
 <template>
   <div class="grid grid-cols-2 gap-8">
     <div>
       <h1>My Greenhouse: {{ id }}</h1>
-      <StatsSingleGreenhouse/>
-        </div>
+      <StatsCardNew />
+      <!--<ProfileOptionsAndInfo :user="user" />-->
+      <!--<StatsSingleGreenhouse :greenhouse-stats="greenhouseStats" />-->
+    </div>
   </div>
 </template>
